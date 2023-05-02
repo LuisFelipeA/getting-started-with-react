ec3c7557d2520ca1a2d78501c9237f3de6b6056b
Luis Felipe <felipeadriani2@gmail.com>
2023-05-02 17:06:56 -0300

    Curso Celke

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React, { Component } from 'react'
-import Table from './Table'
+import React from 'react'
 
-class App extends Component {
-    render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
-  
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
-  }
+function App() {
+  return (
+    <div>
+      Ola
+    </div>
+  )
+}
 
-  export default App
+export default App
